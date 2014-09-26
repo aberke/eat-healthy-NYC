8d74b19b0e9e49c66bb03da995670a57357e5786
Alexandra Berke <alexandra_berke@brown.edu>
2014-09-25 22:07:38 -0400

    clean up

--- app/static/js/menu-controller.js
+++ app/static/js/menu-controller.js
@@ -14,23 +14,17 @@
 *********************************************************************/
 
 
-function sayHi() {
-	console.log("hi")
-}
-
 var mainViewContainer = {
 	'element': null,
 	'collapsed': false
 };
 
 function initMenuController() {
-	console.log('menu-controller initialize')
 	
 	mainViewContainer.element = document.getElementById('main-view-container');
 }
 
 function toggleMenu() {
-	console.log('toggleMenu')
 
 	if (mainViewContainer.collapsed) {
 		mainViewContainer.element.className = '';
