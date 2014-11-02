dd67e8b0ef4bbaa7169bf52ebaa411133a5eba8e
Alexandra Berke <alexandra_berke@brown.edu>
2014-11-02 17:24:00 -0500

    hooked up api PUT and POST

--- app/static/admin/admin-controllers.js
+++ app/static/admin/admin-controllers.js
@@ -1,6 +1,6 @@
 
 // wraps around all of admin-base
-AdminMainController = function($scope, $window, APIservice) {
+AdminMainController = function($scope, $window, $location, APIservice) {
 
 	console.log("AdminMainController")
 
@@ -19,6 +19,7 @@ AdminMainController = function($scope, $window, APIservice) {
 
 	/* menu controllers */
 	$scope.goToAdmin = function() {
+		$location.path('/admin');
 		$scope.menuActive = false;
 	}
 	$scope.goToMap = function() {
@@ -27,10 +28,39 @@ AdminMainController = function($scope, $window, APIservice) {
 }
 // detail admin view is where markets are edited
 AdminDetailController = function($scope, $routeParams, APIservice) {
-	console.log("AdminDetailController", $routeParams, $scope.markets)
-	$scope.market = $scope.markets[$routeParams.marketId];
 
-	$scope.testDate = new Date(1970,0,1,14,57,0);
+	$scope.market = {};
+
+	function initalizeMarket() {
+		// if /admin/detail/new --> leave market as empty object in edit mode
+		if ($routeParams.marketId == 'new') {
+			$scope.editMode = true;
+			return;
+		}
+		$scope.market = $scope.markets[$routeParams.marketId];
+	}
+
+	$scope.editMode = false;
+	$scope.edit = function() {
+		$scope.editMode = true;
+	}
+	$scope.save = function() {
+		// if market has _id: make PUT, else: make POST
+		console.log('save', $scope.market)
+
+		if($scope.market._id) {
+			APIservice.PUT('/markets/' + $scope.market._id, $scope.market).then(function(ret) {
+				$scope.editMode = false;
+			});
+		} else {
+			APIservice.POST('/markets', $scope.market).then(function(ret) {
+				$scope.market._id = ret.data._id;
+				$scope.editMode = false;
+				console.log('$scope.market', $scope.market)
+			});
+		}
+
+	}
 
 	$scope.scheduleDays = ['1','2','3','4','5','6','7'];
 	$scope.scheduleDaysNames = {
@@ -43,7 +73,7 @@ AdminDetailController = function($scope, $routeParams, APIservice) {
 		'7':'SATURDAY'
 	}
 
-
+	initalizeMarket();
 	console.log("market", $scope.market)
 }
 
@@ -52,6 +82,9 @@ AdminDashboardController = function($scope, $location) {
 
 	$scope.searchActive = false;
 
+	$scope.newMarket = function() {
+		$location.path("/admin/detail/new");
+	}
 	$scope.goToDetail = function(market) {
 		$location.path("/admin/detail/" + market._id);
 	}
