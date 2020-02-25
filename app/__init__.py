a944876bb8d858dc7d2d598a8af383066b044204
aberke <berke.alexandra@gmail.com>
2020-02-25 17:30:19 -0500

    updated requirements. But will no longer be using python. this is now a static page with old data

--- app/__init__.py
+++ app/__init__.py
@@ -11,7 +11,7 @@
 #*********************************************************************************
 
 from flask import Flask, send_file, request
-from flask.ext.compress import Compress
+from flask_compress import Compress
 import json
 from util import respond500, respond200
 from basic_auth import requires_auth
